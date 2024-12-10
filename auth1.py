e28ca8601d9b16992ccc6b07d4c4082e4106598d
HeenaJamal <138366408+HeenaJamal@users.noreply.github.com>
2024-12-10 18:31:55 +0530

    Update auth1.py

--- auth1.py
+++ auth1.py
@@ -10,4 +10,4 @@ api_key_header = APIKeyHeader(name="Authorization")
 def get_admin_user(api_key: str = Security(api_key_header)):
     if api_key != API_KEY:
         raise HTTPException(status_code=403, detail="Not authorized")
-    return "admin".
+    return "admin"
